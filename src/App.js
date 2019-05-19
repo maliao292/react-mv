71dc024df7df91f132fe8624bf6b9a890b39fa64
Mario <maming292@126.com>
2019-05-19 16:40:00 +0800

    ceshi

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Home from '';
+function App() {
+  return (
+   
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Home extends Comeponent {
+    // constructor() { }
+    render(){
+        return (
+            <div>Home</div>
+        )
+    }
+}
