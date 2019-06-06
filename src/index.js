8aee4b023994fadcf71a2494a632885f0daa3ae9
mario <maming292@163.com>
2019-06-06 12:54:00 +0800

    chuci

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from './serviceWorker'; // 加快react 运行速度 文件
 
+ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
