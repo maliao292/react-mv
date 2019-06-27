a7e40620fcce2bfd5fd7566adddb12aa17f15869
mario <maming292@163.com>
2019-06-27 13:51:18 +0800

    雨女无瓜

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import React, { Component } from 'react';
 // import New from './components/New';
 import List from './components/List'
 import TodoList from './components/TodoList'
+import Reactform from './components/ReactForm'
 class App extends Component {
   render(){
      return (
       <div className="App">
         <List></List>
         <TodoList></TodoList>
+      <Reactform></Reactform>
         {/* <Home></Home>
         <New></New> */}
     </div>
---
+++ src/components/ReactForm.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class ReactForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            msg:'react表单'
+          };
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default ReactForm;
