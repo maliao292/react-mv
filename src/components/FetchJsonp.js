4e711794f7d56707048a15cfb3b231b825c6e708
mario <maming292@163.com>
2019-07-12 11:32:51 +0800

    生命周期

--- src/components/FetchJsonp.js
+++ src/components/FetchJsonp.js
@@ -4,11 +4,24 @@ import fetchJsonp from 'fetch-jsonp';
 class FetchJsonp extends Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = { 
+            list:[]
+         };
+    }
+    getJsonpData = ()=>{
+        let api = 'http://www.phonegap100.com/appapi.php?a=getPortalList&catid=10';
+        fetchJsonp(api).then((response)=>{
+            return response.json()
+            }).then((json)=>{
+                console.log(json.result)
+            })
     }
     render() {
         return (
-            
+           <div>
+               <h2>FetchJsonp</h2>
+               <button onClick={this.getJsonpData}>getJsonpData</button>
+           </div> 
         );
     }
 }
--- src/components/New.js
+++ src/components/New.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './Header'
 import Footer from './Footer'
 import Axios from './Axios';
+import FetchJsonp from './FetchJsonp'
 class News extends Component {  // 继承Component 的第二种写法
     constructor(props) {
         super(props);
@@ -22,6 +23,7 @@ class News extends Component {  // 继承Component 的第二种写法
     render(){
         return (
             <div>
+                <FetchJsonp></FetchJsonp>
                 <Axios></Axios>
                 <Header msg={this.state.msg} fun={this.toHeaderFun} getChildDate={this.getChildDate} title={222}></Header>
                 <Footer ref='footer'></Footer>
